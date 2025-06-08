09abe4520cb3be7d19ef2343ef3045278bb44e2a
Jorge Cambra <jcambrac@gmail.com>
2025-06-08 02:26:44 -0400

    trying making it work with strapi cloud

--- config/plugins.js
+++ config/plugins.js
@@ -13,22 +13,22 @@ module.exports = ({ env }) => ({
       },
     },
   },
-  upload: {
-    config: {
-      provider: '@strapi/provider-upload-aws-s3',
-      providerOptions: {
-        s3Options: {
-          accessKeyId: env('DO_SPACES_ACCESS_KEY'),
-          secretAccessKey: env('DO_SPACES_SECRET_KEY'),
-          endpoint: env('DO_SPACES_ENDPOINT'),
-          region: env('DO_SPACES_REGION'),
-        },
-        baseUrl: `https://${env('DO_SPACES_BUCKET')}.${env('DO_SPACES_REGION')}.cdn.digitaloceanspaces.com`,
-        params: {
-          Bucket: env('DO_SPACES_BUCKET'),
-          ACL: 'public-read',
-        },
-      },
-    },
-  },
+  // upload: {
+  //   config: {
+  //     provider: '@strapi/provider-upload-aws-s3',
+  //     providerOptions: {
+  //       s3Options: {
+  //         accessKeyId: env('DO_SPACES_ACCESS_KEY'),
+  //         secretAccessKey: env('DO_SPACES_SECRET_KEY'),
+  //         endpoint: env('DO_SPACES_ENDPOINT'),
+  //         region: env('DO_SPACES_REGION'),
+  //       },
+  //       baseUrl: `https://${env('DO_SPACES_BUCKET')}.${env('DO_SPACES_REGION')}.cdn.digitaloceanspaces.com`,
+  //       params: {
+  //         Bucket: env('DO_SPACES_BUCKET'),
+  //         ACL: 'public-read',
+  //       },
+  //     },
+  //   },
+  // },
 });
